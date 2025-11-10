5426e39473ce829de699e4550d5e8aaa6b481b0b
Ray <25377131@buaa.edu.cn>
2025-11-10 15:17:00 +0800

    2025.10

--- P1080 [NOIP 2012 提高组] 国王游戏/FileName.cpp
+++ P1080 [NOIP 2012 提高组] 国王游戏/FileName.cpp
@@ -1,10 +1,6 @@
 #include<stdio.h>
 long long arr[1000], brr[1000], crr[1000], rank[1000];
 long long max(long long, long long);
-void f(int,int);
-int len(int[]);
-int fin[1000];
-int temp[1000];
 void ex(int,int);
 int main(void) {
 	int n;
@@ -16,17 +12,13 @@ int main(void) {
 		crr[i] = (arr[i] - 1) * brr[i];
 		rank[i] = i;
 	}
+	int div = brr[rank[n - 1]];
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < n - i - 1; j++) {
 			if (crr[rank[j]] > crr[rank[j + 1]]) ex(j, j + 1);
 		}
 	}
-	for (i = 0; i < 1000; i++) fin[i] = 0;
-	for (i = 0; i < 1000; i++) temp[i] = 0;
-	fin[0] = 1;
-	
 
-	printf("%lld", fin);
 	return 0;
 }
 void ex(int i, int j) {
@@ -37,14 +29,4 @@ void ex(int i, int j) {
 }
 long long max(long long a, long long b) {
 	return a > b ? a : b;
-}
-int len(int arr[1000]) {
-	int i;
-	for (i = 999; i >= 0; i--) {
-		if (arr[i] != 0) break;
-	}
-	return i;
-}
-void f(int a, int b) {
-
 }
